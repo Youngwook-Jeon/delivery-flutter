6824637dd2f3d46aea2b2d13600899dcd2af95ab
Youngwook-Jeon <ela87@naver.com>
2024-02-04 23:49:55 +0900

    Add our custom app bar

---
+++ foodly/lib/common/custom_appbar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodly/common/app_style.dart';
+import 'package:foodly/common/reusable_text.dart';
+import 'package:foodly/constants/constants.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 6.h),
+      height: 110.h,
+      width: width,
+      color: kOffWhite,
+      child: Container(
+        margin: EdgeInsets.only(top: 20.h),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                CircleAvatar(
+                  radius: 22.r,
+                  backgroundColor: kSecondary,
+                  backgroundImage: const NetworkImage(
+                      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 6.h, left: 8.w),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ReusableText(
+                          text: "Deliver to",
+                          style: appStyle(13, kSecondary, FontWeight.w600)),
+                      SizedBox(
+                        width: width * 0.65,
+                        child: Text(
+                          "364 Locust View Drive, San Francisco",
+                          overflow: TextOverflow.ellipsis,
+                          style: appStyle(11, kGrayLight, FontWeight.normal),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              getTimeOfDay(),
+              style: const TextStyle(fontSize: 35),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  String getTimeOfDay() {
+    DateTime now = DateTime.now();
+    int hour = now.hour;
+
+    if (hour >= 0 && hour < 12) {
+      return ' ☀️ ';
+    } else if (hour >= 12 && hour < 16) {
+      return ' ⛅ ';
+    } else {
+      return ' 🌙 ';
+    }
+  }
+}
--- foodly/lib/views/home/home_page.dart
+++ foodly/lib/views/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodly/common/custom_appbar.dart';
 import 'package:foodly/common/custom_container.dart';
 import 'package:foodly/constants/constants.dart';
 
@@ -12,9 +13,7 @@ class HomePage extends StatelessWidget {
       backgroundColor: kPrimary,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(130.h),
-        child: Container(
-          height: 130,
-        ),
+        child: const CustomAppBar(),
       ),
       body: SafeArea(child: CustomContainer(containerContent: Container())),
     );
