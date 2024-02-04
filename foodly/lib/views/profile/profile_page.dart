9bcc771dbbd3401d55c91959017d04f6192f2f9d
Youngwook-Jeon <ela87@naver.com>
2024-02-04 21:52:02 +0900

    Add a custom container

--- foodly/lib/views/profile/profile_page.dart
+++ foodly/lib/views/profile/profile_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodly/common/custom_container.dart';
+import 'package:foodly/constants/constants.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -6,10 +9,14 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
+      backgroundColor: kPrimary,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(130.h),
+        child: Container(
+          height: 130,
+        ),
       ),
-      body: const Center(child: Text('Profile Page')),
+      body: SafeArea(child: CustomContainer(containerContent: Container())),
     );
   }
 }
