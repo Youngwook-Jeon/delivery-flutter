c296467ddc828dd3ec94f88cca19e791be46e1a7
Youngwook-Jeon <ela87@naver.com>
2024-03-29 19:55:01 +0900

    Add a category / Get categories

--- backend-foodly/controllers/categoryController.js
+++ backend-foodly/controllers/categoryController.js
@@ -12,4 +12,37 @@ module.exports = {
       res.status(500).json({ status: false, message: error.message });
     }
   },
+
+  getAllCategories: async (req, res) => {
+    try {
+      const categories = await Category.find(
+        { title: { $ne: "More" } },
+        { __v: 0 }
+      );
+      res.status(200).json(categories);
+    } catch (error) {
+      res.status(500).json({ status: false, message: error.message });
+    }
+  },
+
+  getRandomCategories: async (req, res) => {
+    try {
+      let categories = await Category.aggregate([
+        { $match: { value: { $ne: "more" } } },
+        { $sample: { size: 4 } },
+      ]);
+
+      const moreCategory = await Category.findOne(
+        { value: "more" },
+        { __v: 0 }
+      );
+      if (moreCategory) {
+        categories.push(moreCategory);
+      }
+
+      res.status(200).json(categories);
+    } catch (error) {
+      res.status(500).json({ status: false, message: error.message });
+    }
+  },
 };
--- backend-foodly/server.js
+++ backend-foodly/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const categoryRoute = require("./routes/categoryRoute");
 
 const port = process.env.PORT || 8000;
 
@@ -12,7 +13,10 @@ mongoose
   .then(() => console.log("DB connection has been established."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/categories", categoryRoute);
+
 app.listen(port, () =>
   console.log(`Backend for Foodly is running on port ${port}.`)
 );
